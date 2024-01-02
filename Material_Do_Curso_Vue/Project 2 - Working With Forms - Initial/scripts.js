c91e4a4541ef6c718e71a7ae7783df5180af6087
Luiza <ludpaiva@gmail.com>
2024-01-02 10:22:25 -0300

    Diretiva v-model

--- Material_Do_Curso_Vue/Project 2 - Working With Forms - Initial/scripts.js
+++ Material_Do_Curso_Vue/Project 2 - Working With Forms - Initial/scripts.js
@@ -15,10 +15,20 @@ const handlingForms = {
 
     data() {
         return {
-            members: window.members
+            members: window.members,
+            newMember: {}
         }
     },
-
+    methods: {
+        addMember: function() {
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+            this.members.push(this.newMember);
+            this.newMember={};
+            } else {
+            alert("Todos os campos devem ser preenchidos.");
+            }
+        }
+    }
 };
 
 Vue.createApp(handlingForms).mount('#app');
